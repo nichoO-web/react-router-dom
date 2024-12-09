df455924306f6eb1f9f9c56b70742e28dba863f2
nichoOtero <nichonaks@gmail.com>
2024-12-08 21:55:31 -0700

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 // src/App.jsx
+import { useState } from 'react';
+import PokemonList from './components/PokemonList';
+import NavBar from './components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import PokemonDetails from './components/PokemonDetails';
+import { useParams } from 'react-router-dom';
+import PokemonForm from './components/PokemonForm';
+
+const initialState = [
+  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
+  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
+  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
+  { _id: 4, name: 'charmander', weight: 85, height: 6 },
+  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+];
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pokemon, setPokemon] = useState(initialState);
+  const addPokemon = (newPokemonData) => {
+    newPokemonData._id = pokemon.length + 1;
+    setPokemon([...pokemon, newPokemonData]);
+  };
+  return (
+    <>
+      <h1>Pokemon!</h1>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<h2>Home Page</h2>} /> 
+        <Route path='/pokemon' element={<PokemonList pokemon={pokemon} />} />
+        <Route path='/pokemon/:pokemonId' element={<PokemonDetails pokemon={pokemon}/>} />
+        <Route path='/pokemon/new' element={<PokemonForm addPokemon={addPokemon} />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/PokemonDetails.jsx
@@ -0,0 +1,23 @@
+// src/components/PokemonDetails.jsx
+
+import { useParams } from "react-router-dom";
+
+const PokemonDetails = (props) => {
+    const { pokemonId } = useParams();
+    const singlePokemon = props.pokemon.find((poke) => poke._id === Number(pokemonId));
+    console.log('Pokemon Object:', singlePokemon)
+    return (
+      <>
+        <h2>{singlePokemon.name}</h2>
+        <dl>
+          <dt>Weight:</dt>
+          <dd>{singlePokemon.weight}</dd>
+          <dt>Height:</dt>
+          <dd>{singlePokemon.height}</dd>
+        </dl>
+      </>
+    );
+  };
+  
+  export default PokemonDetails;
+  
